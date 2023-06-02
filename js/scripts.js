74e98dab14d0c56d3980bb24331d49e9a0c3cb63
Allyson Camp <Allysoncamp92@gmail.com>
2023-06-02 12:06:47 -0700

    WIP begin writing an onload function

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,13 @@
 // onload function
+window.onload = function () {
+  const form = document.querySelector("form");
 
+  form.onsubmit = function (e) {
+    e.preventdefault();
+    let questions = document.getElementById("questions");
+    let suggestion = document.getElementById("suggestion");
+
+    questions.setAttribute("class", "hidden");
+    suggestions.setAttribute("class", "hidden");
+  };
+};
