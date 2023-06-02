2a90e1125ef5140cab869c695901a45fda692ea9
Allyson Camp <Allysoncamp92@gmail.com>
2023-06-02 15:51:04 -0700

    Grab all dom elements needed for onsubmit function, delete the third answer option from question divs

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,28 @@
 // onload function
 window.onload = function () {
-    let form = document.querySelector("form");
+    const form = document.querySelector("form");
 
-    form.onsubmit = function (e) {
-        e.preventdefault();
-        let questions = document.getElementById("questions");
-        let suggestion = document.getElementById("suggestion");
-        // const nameInput = document.querySelector("input#name-input").value;
-
-        questions.setAttribute("class", "hidden");
-        // document.questions.setAttribute("class", "hidden");
-        //suggestion.setAttribute("class", "hidden");
+  form.onsubmit = function (e) {
+    e.preventdefault();
+    let questions = document.getElementById("questions");
+    let pomegranate = document.getElementById("pomegranate");
+    let grapes = document.getElementById("grapes");
+    let brussels = document.getElementById("brussels");
+    let potatoes = document.getElementById("potatoes");
+    let crunchy = document.getElementById("crunchy");
+    let chewy = document.getElementById("chewy");
+    let jamacian = document.getElementById("jamacian");
+    let greek = document.getElementById("greek");
       
-      questions.removeAttribute("class");
-      suggestion.removeAttribute("class");
-     
-    };
+    let suggestion = document.getElementById("suggestion");
+    let html = document.getElementById("html");
+    let cSharp = document.getElementById("c#");
+    let css = document.getElementById("css");
+      
+    // const nameInput = document.querySelector("input#name-input").value;
+
+    // questions.setAttribute("class", "hidden");
+    // document.questions.setAttribute("class", "hidden");
+    //suggestion.setAttribute("class", "hidden");
+  }
 };
