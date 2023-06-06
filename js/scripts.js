1e32679010372a71184e0167f9474b215c39848e
Allyson Camp <Allysoncamp92@gmail.com>
2023-06-05 20:34:06 -0700

    WIP if/else statements partially functioning

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,59 @@
+function customMessage() {
+    // const customMessageDiv = document.getElementById("custom-message");
+    const nameInput = document.getElementById("name-input").value;
+    // customMessageDiv.setAttribute("class", "hidden");
+    document.querySelector("span#person-name").innerText = nameInput;
+    document.querySelector("div#custom-message").removeAttribute("class");
+    //makes user name appear in custom message
+}
+
+function questionsFunction() {
+    const questionForm = document.querySelector("form#questions");
+    //?one
+    questionForm.onsubmit = function (e) {
+        e.preventDefault();
+
+        let htmlLang = document.getElementById("html-lang");
+        htmlLang.setAttribute("class", "hidden");
+        let cssLang = document.getElementById("css-lang");
+        cssLang.setAttribute("class", "hidden");
+        let cLang = document.getElementById("c-lang");
+        cLang.setAttribute("class", "hidden");
+        //one
+        const question1 = document.getElementById("question-one-div");
+        let grapes = document.getElementById("grapes");
+        let pomegranate = document.getElementById("pomegranate");
+
+        //two
+        const question2 = document.getElementById("question-two");
+        let fried = document.getElementById("fried");
+        let salad = document.getElementById("salad");
+        //three
+        //four
+        //five
+        //branching statements
+        if (grapes || salad) {
+            document.querySelector("div#html-lang").removeAttribute("class");
+        } else if (fried || pomegranate) {
+            document.querySelector("div#css-lang").removeAttribute("class");
+        } else {
+          document.querySelector("div#c-lang").removeAttribute("class");
+        }
+
+      //   if (fried) {
+      //       document.querySelector("div#html-lang").removeAttribute("class");
+      //   } else {
+      //       document.querySelector("div#c-lang").removeAttribute("class");
+      //   }
+      
+      //  if (salad) {
+      //      document.querySelector("div#css-lang").removeAttribute("class");
+      //  } else {
+      //      document.querySelector("div#c-lang").removeAttribute("class");
+      //  }
+    };
+}
+
 //opnload function
 window.onload = function () {
     const form = document.querySelector("form");
@@ -5,32 +61,10 @@ window.onload = function () {
     form.onsubmit = function (e) {
         e.preventDefault();
         //make name appear
-      customMessage();
-      questionOne();
+        customMessage();
+        questionsFunction();
     };
 };
-
-function customMessage() {
-    // const customMessageDiv = document.getElementById("custom-message");
-    const nameInput = document.getElementById("name-input").value;
-    // customMessageDiv.setAttribute("class", "hidden");
-    document.querySelector("span#person-name").innerText = nameInput;
-  document.querySelector("div#custom-message").removeAttribute("class");
-  //makes user name appear in custom message
-}
-
-function questionOne() {
-    const question1 = document.getElementById("question-one");
-    let grapes = document.getElementById("grapes");
-    let pomegranate = document.getElementById("pomegranate");
-
-  
-    if (question1 === grapes) {
-        document.querySelector("div#html").removeattribute("class");
-    } else if (question1 === pomegranate) {
-        document.querySelector("div#css").removeAttribute("class");
-    }
-}
 
 // let questions = document.getElementById("questions");
 // let pomegranate = document.getElementById("pomegranate");
