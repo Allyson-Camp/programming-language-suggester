aac4ad335d2e5931905111158999d6e149024dc8
Allyson Camp <Allysoncamp92@gmail.com>
2023-06-07 13:08:51 -0700

    Clean up unneeded comments in js

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,9 @@ function customMessage() {
     // customMessageDiv.setAttribute("class", "hidden");
     document.querySelector("span#person-name").innerText = nameInput;
     document.querySelector("div#custom-message").removeAttribute("class");
-    //makes user name appear in custom message
+    //if (nameInput === "") or if !nameInput?? {
+    // remove attribute of error message
+    // }
 }
 
 function questionsFunction() {
@@ -32,12 +34,10 @@ function questionsFunction() {
         //four
         //five
         //branching statements
-        if (grapes || salad) {
+        if (grapes) {
             document.querySelector("div#html-lang").removeAttribute("class");
-        } else if (fried || pomegranate) {
-            document.querySelector("div#css-lang").removeAttribute("class");
         } else {
-          document.querySelector("div#c-lang").removeAttribute("class");
+            document.querySelector("div#css-lang").removeAttribute("class");
         }
 
       //   if (fried) {
@@ -65,59 +65,3 @@ window.onload = function () {
         questionsFunction();
     };
 };
-
-// let questions = document.getElementById("questions");
-// let pomegranate = document.getElementById("pomegranate");
-// let grapes = document.getElementById("grapes");
-// let salad = document.getElementById("salad");
-// let brussels = document.getElementById("brussels");
-// let potatoes = document.getElementById("potatoes");
-// let crunchy = document.getElementById("crunchy");
-// let chewy = document.getElementById("chewy");
-// let jamacian = document.getElementById("jamacian");
-// let greek = document.getElementById("greek");
-// let question1 = document.getElementById("question-one");
-// let question2 = document.getElementById("question-two");
-// let question3 = document.getElementById("question-three")
-
-// let suggestion = document.getElementById("suggestion");
-// let html = document.getElementById("html");
-// let cSharp = document.getElementById("c#");
-// let css = document.getElementById("css");
-
-// const nameInput = document.querySelector("input#name-input").value;
-
-// html.setAttribute("class", "hidden");
-// document.questions.setAttribute("class", "hidden");
-// suggestion.setAttribute("class", "hidden");
-
-//     if (question1 === grapes) {
-//       // document.querySelector("div#html").removeattribute("class");
-//       document.querySelector("div#html").removeAttribute("class");
-//     } else if (question1 === pomegranate) {
-//       document.querySelector("div#css").removeAttribute("class");
-//     }
-//   }
-
-//   if (question2 === fried) {
-//     document.querySelector.cSharp.removeAttribute("hidden");
-//   } else if (question2 === salad) {
-//     document.querySelector.html.removeAttribute("class");
-//   }
-
-//   if (question3 === brussels) {
-//       document.querySelector.cSharp.removeAttribute("hidden");
-//   } else if (question3 === potatoes) {
-//       document.querySelector.css.removeAttribute("class");
-//   }
-
-//   if (question4 === crunchy) {
-//       document.querySelector.css.removeAttribute("hidden");
-//   } else if (question4 === chewy) {
-//       document.querySelector.html.removeAttribute("class");
-//   }
-
-//   if (question5 === jamacian) {
-//       document.querySelector.html.removeAttribute("hidden");
-//   } else if (question5 === greek) {
-//       document.querySelector.html.removeAttribute("class");
