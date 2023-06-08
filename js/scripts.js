42205afd6e97783ebcf0a8b56896bf5f4f5e54bf
Allyson Camp <Allysoncamp92@gmail.com>
2023-06-07 17:59:04 -0700

    Create seperate languageSuggestion function

--- js/scripts.js
+++ js/scripts.js
@@ -1,66 +1,53 @@
 function customMessage() {
     // const customMessageDiv = document.getElementById("custom-message");
     const nameInput = document.getElementById("name-input").value;
-    // customMessageDiv.setAttribute("class", "hidden");
     document.querySelector("span#person-name").innerText = nameInput;
     document.querySelector("div#custom-message").removeAttribute("class");
-    //if (nameInput === "") or if !nameInput?? {
-    // remove attribute of error message
-    // }
+}
+
+function languageSuggestion() {
+    const htmlLang = document.getElementById("html-lang");
+    htmlLang.setAttribute("class", "hidden");
+    const cssLang = document.getElementById("css-lang");
+    cssLang.setAttribute("class", "hidden");
+    const cLang = document.getElementById("c-lang");
+    cLang.setAttribute("class", "hidden");
 }
 
 function questionsFunction() {
     const form = document.querySelector("form");
 
     form.addEventListener("submit", function (e) {
-        e.preventDefault();//needed or not bc its already in the load???
+        e.preventDefault(); //needed or not bc its already in the load???
+        languageSuggestion();
 
-        const htmlLang = document.getElementById("html-lang");
-        htmlLang.setAttribute("class", "hidden");
-        const cssLang = document.getElementById("css-lang");
-        cssLang.setAttribute("class", "hidden");
-        const cLang = document.getElementById("c-lang");
-        cLang.setAttribute("class", "hidden");
-       
         //question one
-        const fruitQuestion = document.getElementById("fruitQuestion");
-        const grapes = document.getElementById("grapes");
-        const pomegranate = document.querySelector("pomegranate")
+        const fruitQuestion = document.querySelector(
+            "input[name='fruitQuestion']"
+        ); //how do you grab the element
+        const grapes = document.querySelector("input[name='grapes']");
 
-        if (fruitQuestion === pomegranate) {
-            document.querySelector("div#html-lang").removeAttribute("class");
+        const typeQuestion = document.querySelector(
+            "input[name='typeQuestion']"
+        );
+        const fried = document.querySelector("input[name='fried']");
+
+        if (fruitQuestion === grapes) {
+            document.querySelector("div#c-lang").removeAttribute("class");
         } else {
-            document.querySelector("div#css-lang").removeAttribute("class");
+            document.querySelector("div#html-lang").removeAttribute("class");
         }
 
         if (typeQuestion === fried) {
-            document.querySelector("div#html-lang").removeAttribute("class");
+            document.querySelector("div#css-lang").removeAttribute("class");
         } else {
             document.querySelector("div#c-lang").removeAttribute("class");
         }
-
-          if (typeQuestion === fried) {
-              document.querySelector("div#html-lang").removeAttribute("class");
-          } else {
-              document.querySelector("div#c-lang").removeAttribute("class");
-          }
-        
-          if (typeQuestion === fried) {
-              document.querySelector("div#html-lang").removeAttribute("class");
-          } else {
-              document.querySelector("div#c-lang").removeAttribute("class");
-          }
-        
-          if (typeQuestion === fried) {
-              document.querySelector("div#html-lang").removeAttribute("class");
-          } else {
-              document.querySelector("div#c-lang").removeAttribute("class");
-          }
     });
 }
 
 //opnload function
-window.addEventListener ("load", function () {
+window.addEventListener("load", function () {
     const form = document.querySelector("form");
 
     form.addEventListener("submit", function (e) {
